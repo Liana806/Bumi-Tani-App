821cb8955efccf9dfa2fd45243861f0d5ffc07cb
Chrstvn21 <21xhrtvn@gmail.com>
2024-06-27 12:54:47 +0800

    splash scr

--- app/src/main/java/com/example/bumitani/MainActivity.java
+++ app/src/main/java/com/example/bumitani/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bumitani;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button introBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
